afb94505670dfacb7c28ab15933f153a48cf2594
Viku1728 <145371635+Viku1728@users.noreply.github.com>
2023-09-18 21:18:54 +0530

    Add files via upload

---
+++ WATERCONS.py
@@ -0,0 +1,19 @@
+# cook your dish here
+// Code for Day 1 
+#include<stdio.h>
+
+int main() {
+// Code for sum of two numbers given by user
+    int a;
+    int b;
+    int sum;
+    
+    printf("Enter first number a : ");
+    scanf("%d", &a);
+    printf("Enter second number b : ");
+    scanf("%d", &b);
+
+    sum = a + b;
+    printf("Sum is : %d", sum);
+    return 0;
+}
